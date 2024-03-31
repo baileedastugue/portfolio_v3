cebae3fa13f725d90651bc52f94f0575eeee2dd0
Bailee Dastugue <bailee.dast@gmail.com>
2024-03-31 07:00:12 -0500

    pivot to vite - set up and initial files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-installGlobals();
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-});
+  plugins: [react()],
+})
