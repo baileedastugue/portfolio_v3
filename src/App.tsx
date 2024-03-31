bf3e039928d40f21f067672ef97aefdd1a99dbc6
Bailee Dastugue <bailee.dast@gmail.com>
2024-03-31 08:28:40 -0500

    adding prettier plugin to automatically sort tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        push the button
+      </button>
+    </>
+  );
 }
 
 export default App;
