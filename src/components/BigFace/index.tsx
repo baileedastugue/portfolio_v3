2936dcd1c6409cadd91cebbf4e49576df07c7f17
Bailee Dastugue <bailee.dast@gmail.com>
2024-04-21 20:47:47 -0500

    bailee/landing: play/plause animation functionality - not pretty

--- src/components/BigFace/index.tsx
+++ src/components/BigFace/index.tsx
@@ -1,27 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Faces from '../../assets/Faces';
+import Pause from '../../assets/svgs/Pause';
+import Play from '../../assets/svgs/Play';
 
 const BigFace = () => {
 	const [currFaceIndex, setCurrFaceIndex] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(true);
+
+	const intervalId = useRef(0);
 
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setCurrFaceIndex(currFaceIndex => {
-				const faceIndex =
-					currFaceIndex === Faces.length - 1 ? 0 : currFaceIndex + 1;
-				return faceIndex;
-			});
+		intervalId.current = setInterval(() => {
+			if (isPlaying) {
+				setCurrFaceIndex(currFaceIndex => {
+					const faceIndex =
+						currFaceIndex === Faces.length - 1 ? 0 : currFaceIndex + 1;
+					return faceIndex;
+				});
+			} else {
+				return () => clearInterval(intervalId.current);
+			}
 		}, 1500);
-
-		return () => clearInterval(interval);
-	}, []);
+		return () => clearInterval(intervalId.current);
+	}, [isPlaying]);
 
 	return (
-		<img
-			className='md:bg-clip-circle md:circle-clip absolute -bottom-28 -right-20 min-w-full-plus-5rem sm:-bottom-20 md:-bottom-32 md:-right-10 md:w-2/3 md:min-w-0'
-			alt='Bailee Dastugue; woman with brown hair smiling behind a computer.'
-			src={Faces[currFaceIndex]}
-		/>
+		<div className='absolute -bottom-28 -right-20 min-w-full-plus-5rem sm:-bottom-20 md:-bottom-32 '>
+			<button
+				onClick={() => {
+					setIsPlaying(curr => !curr);
+				}}
+				className='absolute left-4 top-32 z-30 w-8 text-white'
+			>
+				{isPlaying ? <Pause /> : <Play />}
+			</button>
+			<img
+				className='md:bg-clip-circle md:circle-clip md:-right-10 md:w-2/3 md:min-w-0 '
+				alt='Bailee Dastugue; woman with brown hair smiling behind a computer.'
+				src={Faces[currFaceIndex]}
+			/>
+		</div>
 	);
 };
 
