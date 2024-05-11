c470a1027bcc807cb6f35463bed5fb1779110326
Bailee Dastugue <bailee.dast@gmail.com>
2024-05-11 16:01:01 -0500

    bailee/underConstruction

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,10 +10,10 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
 	const location = useLocation();
 	return (
-		<div className='relative h-full overflow-clip bg-light dark:bg-purple'>
+		<div className='relative h-dvh overflow-clip bg-light dark:bg-purple'>
 			<DisplayText bottomLeftText='Bailee' topRightText='Dastugue' />
 			{location.pathname === '/' && <BigFace />}
-			<div className='relative h-full overflow-auto px-6 pb-6 pt-16 md:z-20 md:pl-32 md:pr-36 md:pt-12 lg:px-28'>
+			<div className='relative h-dvh overflow-auto px-6 pb-6 pt-16 md:z-20 md:pl-32 md:pr-36 md:pt-12 lg:px-28'>
 				{children}
 			</div>
 		</div>
--- src/routes/MyWork/index.tsx
+++ src/routes/MyWork/index.tsx
@@ -1,7 +1,22 @@
 import PageTitle from '../../components/PageTitle';
+import underBunstruction from '../../assets/underBunstruction.png';
 
 const MyWork = () => {
-	return <PageTitle title='My work' />;
+	return (
+		<div>
+			<PageTitle title='My work' />
+			<div className='mt-4 flex flex-col items-center justify-center text-center'>
+				<img
+					src={underBunstruction}
+					className='mx-auto max-h-96'
+					alt='Brown tabby cat photo with an orange-and-white construction cone cartoon over her left ear'
+				/>
+				<p className='mt-4 font-serif text-xl font-bold md:max-w-96'>
+					Under construction, please pardon our dust and check back soon!
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default MyWork;
